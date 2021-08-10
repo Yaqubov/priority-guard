b31970b10114d04e6cd732082d80e6c6696a167b
Tahir <tahiryaqubov87@gmail.com>
2021-08-10 13:49:53 +0400

    first commit

---
+++ document/readme.txt
@@ -0,0 +1,5 @@
+* Email: contact@botble.com
+* Website: botble.com
+* Support center: https://botble.ticksy.com
+* Online documentation: https://docs.botble.com/stories
+* User guide: https://www.evernote.com/l/AUdC_0EKhNJL26AAmQX7uxc-QyqdgoAFS94
---
+++ main/platform/themes/stories/layouts/default.blade.php
@@ -0,0 +1,17 @@
+{!! Theme::partial('header') !!}
+
+<div class="container">
+    @if (Theme::get('pageId') && !BaseHelper::isHomepage(Theme::get('pageId')))
+        <div class="archive-header pt-50">
+            <h2 class="font-weight-900">{{ SeoHelper::getTitle() }}</h2>
+            {!! Theme::partial('breadcrumbs') !!}
+            <div class="bt-1 border-color-1 mt-30 mb-50"></div>
+        </div>
+    @else
+        <div class="mt-30 mb-50"></div>
+    @endif
+
+    {!! Theme::content() !!}
+</div>
+
+{!! Theme::partial('footer') !!}
